4fac99f119ccb57830f7838cd81a88c123143b7e
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-08-30 19:58:17 +0530

    remove setInterval from wallet sse

--- controllers/wallet-controller.js
+++ controllers/wallet-controller.js
@@ -10,30 +10,25 @@ const getWalletByUserEmail = async (req, res, next) => {
   }
 
   let wallet;
-
   res.writeHead(200, SSE_RES_HEADERS);
-
-  setInterval( async () => {
-    try {
-      wallet = await UserWallet.findOne({ email: userEmail })
-    } catch(err) {
-      const error = new Error('Cannot get user\'s wallet', 500);
-      return next(error);
-    }
-    if (!wallet) {
-      const error = new Error('The user does not exists', 404);
-      return next(error)
-    } 
-    res.write(`data: ${JSON.stringify({wallet: wallet.toObject({ getters: true})})}\n\n`);
-  }, 500)
-  // res.json({ wallet: wallet.toObject({ getters: true}) });
+  try {
+    wallet = await UserWallet.findOne({ email: userEmail })
+  } catch(err) {
+    const error = new Error('Cannot get user\'s wallet', 500);
+    return next(error);
+  }
+  if (!wallet) {
+    const error = new Error('The user does not exists', 404);
+    return next(error)
+  } 
+  res.write(`data: ${JSON.stringify({wallet: wallet.toObject({ getters: true})})}\n\n`);
 };
 
 
 const updateUserWallet = async (req,res,next) => {
   const { walletBalance } = req.body;
   const userEmail = req.params.pid;
-  // console.log(walletBalance, userEmail);
+  
   if (walletBalance <= 0) {
     const error = new Error('Not enough balance to make this transaction',500);
     return next(error);
@@ -54,6 +49,7 @@ const updateUserWallet = async (req,res,next) => {
     const error = new Error('Cannot use your wallet',500);
     return next(error);
   }
+  getWalletByUserEmail();
   res.status(200).json({userWallet: userWallet.toObject({getters: true})})
 };
 
